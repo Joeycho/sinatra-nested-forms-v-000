7dd5e6fe129c7985566d913bd2d64864e277c150
Joey Cho <cjy12182000@naver.com>
2019-01-24 11:12:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,28 @@ module FormsLab
     
     
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+ 
+      params[:student][:courses].each do |details|
+        Course.new(details)
+      end
+     
+      @courses = Course.all
+      
       erb :"/pirates/show"
     end
 
   end
+end
+
+post '/student' do
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
 end
