92eb500f1fe263b52630d05a0461700e360f26ae
Joey Cho <cjy12182000@naver.com>
2019-01-24 10:17:45 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all = []
+  end
+  
+
 end
